348aa974c00df933df27b5395c16af62474a97a2
deathcap <deathcap@gmx.co.uk>
2014-03-30 21:19:33 -0700

    Rotate using matrices instead of quaternions. Closes GH-2

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ var glm = require('gl-matrix')
 var vec3 = glm.vec3
 var mat3 = glm.mat3
 var mat4 = glm.mat4
-var quat = glm.quat
-
-var scratch0 = new Float32Array(16)
-var scratch1 = new Float32Array(16)
 
 module.exports = noclip
 
@@ -13,19 +9,15 @@ function noclip(position) {
   if (!(this instanceof noclip)) return new noclip(position)
 
   this.position = position || vec3.create()
-  this.rotation = quat.create()
+  this.rotationX = this.rotationY = this.rotationZ = 0.0
 }
 
 noclip.prototype.view = function(output) {
   if (!output) output = mat4.create()
-  scratch1[0] =
-  scratch1[1] =
-  scratch1[2] = 0
 
-  mat4.fromRotationTranslation(output
-    , quat.conjugate(scratch0, this.rotation)
-    , scratch1
-  )
+  mat4.rotateX(output, output, this.rotationX)
+  mat4.rotateY(output, output, this.rotationY)
+  mat4.rotateZ(output, output, this.rotationZ)
   mat4.translate(output
     , output
     , this.position
@@ -35,16 +27,16 @@ noclip.prototype.view = function(output) {
 }
 
 noclip.prototype.rotateX   = function(angle) {
-  quat.rotateX(this.rotation, this.rotation, angle)
+  this.rotationX += angle
   return this
 }
 
 noclip.prototype.rotateY   = function(angle) {
-  quat.rotateY(this.rotation, this.rotation, angle)
+  this.rotationY += angle
   return this
 }
 
 noclip.prototype.rotateZ   = function(angle) {
-  quat.rotateZ(this.rotation, this.rotation, angle)
+  this.rotationZ += angle
   return this
 }
